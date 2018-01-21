4f417ac3043469ac3d93bdf72ed004ac50c1931a
Kryst <Krystropolis@users.noreply.github.com>
2018-01-20 23:31:05 -0600

    move jquery before javascript so script loading occurs properly, attach event listeners to navigational links

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
-/**
- * toggles the button color between primary and accent colors
- * @param {element} btn - the button to toggle
- */
- function toggleBtnColor(btn){
- 	if(btn.classList.contains('mdl-button--accent')){
- 		btn.classList.replace('mdl-button--accent','mdl-button--primary');
- 	} else {
- 		btn.classList.replace('mdl-button--primary','mdl-button--accent');
- 	}
- }
+// wait for the dom to load
+$(document).ready(function() {
+	/**
+	 * toggles the button color between primary and accent colors
+	 * @param {element} btn - the button to toggle
+	 */
+ 	function toggleBtnColor(btn){
+	 	if(btn.classList.contains('mdl-button--accent')){
+	 		btn.classList.replace('mdl-button--accent','mdl-button--primary');
+	 	} else {
+	 		btn.classList.replace('mdl-button--primary','mdl-button--accent');
+	 	}
+	 }
 
-/**
- * load content into our page
- * @param {element} btn - the button that called the function
- * @param {string} content - file containing content
- */
-function load_(btn,content)
-{
-	toggleBtnColor(btn);
-    $('.content').load(content, function() {
-        if (console) {
-        	console.log(content + ' loaded successfully.');
-        }
-    });
-}	
+	/**
+	 * load content into our page
+	 * @param {element} btn - the button that called the function
+	 * @param {string} content - file containing content
+	 */
+ 	function load_()
+	 {
+		//toggleBtnColor(btn);
+		var content = this.getAttribute('href');
+		$('.content').load(content, function() {
+			if (console) {
+				console.log(content + ' loaded successfully.');
+			}
+		});
+	}	
 
-/**
- * remove content from our page
- * @param {element} btn - the button that called the function
- */
-function unload_(btn)
-{
-    toggleBtnColor(btn);
-    $('.content').empty();
-}	
+	/**
+	 * remove content from our page
+	 * @param {element} btn - the button that called the function
+	 */
+ 	function unload_(btn)
+	 {
+	    //toggleBtnColor(btn);
+	    $('.content').empty();
+	}	
 
+	function navHandler(){
+		var navLinks = document.querySelectorAll('.mdl-navigation__link');
+		for (var i = 0; i < navLinks.length; i++){
+			navLinks[i].addEventListener('click', load_);
+		}
+	}
+
+	navHandler();
+});
