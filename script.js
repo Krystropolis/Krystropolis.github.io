6c86003851496708a739edcd67f2b7472fdc4335
Kryst <Krystropolis@users.noreply.github.com>
2018-01-20 22:59:58 -0600

    added styling to buttons, added footer image

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 /**
+ * toggles the button color between primary and accent colors
+ * @param {element} btn - the button to toggle
+ */
+ function toggleBtnColor(btn){
+ 	if(btn.classList.contains('mdl-button--accent')){
+ 		btn.classList.replace('mdl-button--accent','mdl-button--primary');
+ 	} else {
+ 		btn.classList.replace('mdl-button--primary','mdl-button--accent');
+ 	}
+ }
+
+/**
  * load content into our page
+ * @param {element} btn - the button that called the function
  * @param {string} content - file containing content
  */
-function load_(content)
+function load_(btn,content)
 {
+	toggleBtnColor(btn);
     $('.content').load(content, function() {
         if (console) {
         	console.log(content + ' loaded successfully.');
@@ -13,9 +27,11 @@ function load_(content)
 
 /**
  * remove content from our page
+ * @param {element} btn - the button that called the function
  */
-function unload_()
+function unload_(btn)
 {
+    toggleBtnColor(btn);
     $('.content').empty();
 }	
 
